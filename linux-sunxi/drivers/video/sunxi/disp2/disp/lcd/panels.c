fbbb5cd9aa1dea558aa278c2bd58dc6232fe3f98
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2015-11-27 01:53:41 +0000

    support LCD7 touchscreen

--- linux-sunxi/drivers/video/sunxi/disp2/disp/lcd/panels.c
+++ linux-sunxi/drivers/video/sunxi/disp2/disp/lcd/panels.c
@@ -6,12 +6,14 @@ extern __lcd_panel_t default_panel;
 extern __lcd_panel_t lt070me05000_panel;
 extern __lcd_panel_t wtq05027d01_panel;
 extern __lcd_panel_t t27p06_panel;
+extern __lcd_panel_t S070WV20_MIPI_RGB_panel;
 
 __lcd_panel_t* panel_array[] = {
 	&default_panel,
 	&lt070me05000_panel,
 	&wtq05027d01_panel,
 	&t27p06_panel,
+	&S070WV20_MIPI_RGB_panel,
 	/* add new panel below */
 
 	NULL,
